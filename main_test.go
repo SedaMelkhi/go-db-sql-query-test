9c8f3e23a1a3cd988ffab8bd44c529f03de14ea8
SedaMelkhi <gudieva-seda@mail.ru>
2026-02-06 14:12:19 +0300

    Спринт 8. Глава 3: 01. Тестирование запросов

--- main_test.go
+++ main_test.go
@@ -2,48 +2,66 @@ package main
 
 import (
 	"testing"
-
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	_ "modernc.org/sqlite"
+	"database/sql"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
 	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "demo.db")
+	assert.NoError(t, err)
 	clientID := 1
-
-	// напиши тест здесь
+	client, err := selectClient(db, clientID)
+	assert.NoError(t, err)
+	assert.Equal(t, clientID, client.ID)
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
 	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "demo.db")
+	assert.NoError(t, err)
 	clientID := -1
-
-	// напиши тест здесь
+	_, err = selectClient(db, clientID)
+	require.ErrorIs(t, err, sql.ErrNoRows,  "ожидалась ошибка sql.ErrNoRows")
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "demo.db")
+	assert.NoError(t, err)
 	cl := Client{
 		FIO:      "Test",
 		Login:    "Test",
 		Birthday: "19700101",
 		Email:    "mail@mail.com",
 	}
-
-	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, cl.ID)
+	_, err = selectClient(db, cl.ID)
+	assert.NoError(t, err)
+	
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	// настройте подключение к БД
-
+	db, err := sql.Open("sqlite", "demo.db")
+	assert.NoError(t, err)
 	cl := Client{
 		FIO:      "Test",
 		Login:    "Test",
 		Birthday: "19700101",
 		Email:    "mail@mail.com",
 	}
-
-	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, cl.ID)
+	_, err = selectClient(db, cl.ID)
+	require.NoError(t, err)
+	err = deleteClient(db, cl.ID)
+	require.NoError(t, err)
+	_, err = selectClient(db, cl.ID)
+	require.ErrorIs(t, err, sql.ErrNoRows, "Ожидалась ошибка sql.ErrNoRows")
 }
